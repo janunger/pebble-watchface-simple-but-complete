e6bab393f5297e3e766b0d2311979fdfbb1711eb
Jan Unger <ju@juit.de>
2015-09-04 20:51:04 +0200

    Very basic watchface, hour and minute

--- src/watchface.c
+++ src/watchface.c
@@ -1,60 +1,54 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
+static Window *main_window;
+static TextLayer *time_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+static void update_time() {
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+    static char buffer[] = "00:00";
+    if (clock_is_24h_style() == true) {
+        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    } else {
+        strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    }
+    text_layer_set_text(time_layer, buffer);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static void main_window_load(Window *window) {
+    time_layer = text_layer_create(GRect(0, 55, 144, 50));
+    text_layer_set_background_color(time_layer, GColorClear);
+    text_layer_set_text_color(time_layer, GColorBlack);
+    text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+    layer_add_child(window_get_root_layer(main_window), text_layer_get_layer(time_layer));
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static void main_window_unload(Window *window) {
+    text_layer_destroy(time_layer);
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
-static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-}
-
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+    main_window = window_create();
+    window_set_window_handlers(main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload
+    });
+    window_stack_push(main_window, true);
+    update_time();
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+    window_destroy(main_window);
 }
 
 int main(void) {
-  init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
