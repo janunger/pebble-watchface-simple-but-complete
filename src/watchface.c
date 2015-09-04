e7cb114e228b7bdc65dfeee627ae12cbd21e3ef5
Jan Unger <ju@juit.de>
2015-09-04 21:36:03 +0200

    Displaying seconds and date

--- src/watchface.c
+++ src/watchface.c
@@ -1,19 +1,27 @@
 #include <pebble.h>
 
 static Window *main_window;
-static TextLayer *time_layer;
-static GFont time_font;
+static TextLayer *time_layer, *seconds_layer, *date_layer;
+static GFont time_font, seconds_font, date_font;
 
 static void update_time() {
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
-    static char buffer[] = "00:00";
+    static char time_buffer[] = "00:00";
     if (clock_is_24h_style() == true) {
-        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+        strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
     } else {
-        strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+        strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
     }
-    text_layer_set_text(time_layer, buffer);
+    text_layer_set_text(time_layer, time_buffer);
+
+    static char seconds_buffer[8];
+    strftime(seconds_buffer, sizeof(seconds_buffer), "%S", tick_time);
+    text_layer_set_text(seconds_layer, seconds_buffer);
+
+    static char date_buffer[16];
+    strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
+    text_layer_set_text(date_layer, date_buffer);
 }
 
 static void main_window_load(Window *window) {
@@ -24,10 +32,31 @@ static void main_window_load(Window *window) {
     text_layer_set_font(time_layer, time_font);
     text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
     layer_add_child(window_get_root_layer(main_window), text_layer_get_layer(time_layer));
+
+    seconds_layer = text_layer_create(GRect(0, 90, 144, 34));
+    text_layer_set_background_color(seconds_layer, GColorClear);
+    text_layer_set_text_color(seconds_layer, GColorBlack);
+    seconds_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_NARROW_BOLD_30));
+    text_layer_set_font(seconds_layer, seconds_font);
+    text_layer_set_text_alignment(seconds_layer, GTextAlignmentCenter);
+    layer_add_child(window_get_root_layer(main_window), text_layer_get_layer(seconds_layer));
+
+    date_layer = text_layer_create(GRect(0, 125, 144, 34));
+    text_layer_set_background_color(date_layer, GColorClear);
+    text_layer_set_text_color(date_layer, GColorBlack);
+    date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_NARROW_BOLD_30));
+    text_layer_set_font(date_layer, date_font);
+    text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+    layer_add_child(window_get_root_layer(main_window), text_layer_get_layer(date_layer));
 }
 
 static void main_window_unload(Window *window) {
+    fonts_unload_custom_font(time_font);
+    fonts_unload_custom_font(seconds_font);
+    fonts_unload_custom_font(date_font);
     text_layer_destroy(time_layer);
+    text_layer_destroy(seconds_layer);
+    text_layer_destroy(date_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -42,7 +71,7 @@ static void init(void) {
     });
     window_stack_push(main_window, true);
     update_time();
-    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit(void) {
